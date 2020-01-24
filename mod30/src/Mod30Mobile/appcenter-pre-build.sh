44e9cfd047da57018b5d8e56d24913d4d25a3d77
Matt Soucoup <msoucoup@codemilltech.com>
2020-01-23 17:42:23 -0800

    updating the pre-build script

--- mod30/src/Mod30Mobile/appcenter-pre-build.sh
+++ mod30/src/Mod30Mobile/appcenter-pre-build.sh
@@ -6,7 +6,7 @@ echo " - Android App Center: $ANDROID_APP_CENTER"
 
 # Updating ids
 
-AppCenterConstantsFile=$BUILD_REPOSITORY_LOCALPATH/src/TailwindTraders.Mobile/TailwindTraders.Mobile.Core/Helpers/AppCenterConstants.cs
+AppCenterConstantsFile=$BUILD_REPOSITORY_LOCALPATH/mod30/src/Mod30Mobile/TailwindTraders.Mobile/TailwindTraders.Mobile.Core/Helpers/AppCenterConstants.cs
 
 sed -i '' "s/< ENTER YOUR APP CENTER IOS SECRET HERE >/$IOS_APP_CENTER/g" $AppCenterConstantsFile
 sed -i '' "s/< ENTER YOUR APP CENTER ANDROID SECRET HERE >/$ANDROID_APP_CENTER/g" $AppCenterConstantsFile
@@ -14,4 +14,12 @@ sed -i '' "s/< ENTER YOUR APP CENTER ANDROID SECRET HERE >/$ANDROID_APP_CENTER/g
 # Print out file for reference
 cat $AppCenterConstantsFile
 
-echo "Updated id!"
+echo "Updated App Center Constants"
+
+GoogleJsonFile=$BUILD_REPOSITORY_LOCALPATH/mod30/src/Mod30Mobile/TailwindTraders.Mobile/TailwindTraders.Mobile.Android/google-services.json
+
+sed -i '' "s/< PROJECT NUMBER >/$GOOGLE_PROJ_NUMBER/g" $GoogleJsonFile
+sed -i '' "s/< MOBILE SDK APP ID >/$GOOGLE_MOBILE_SDK_APP_ID/g" $GoogleJsonFile
+sed -i '' "s/< CLIENT ID >/$GOOGLE_CLIENT_ID/g" $GoogleJsonFile
+sed -i '' "s/< CURRENT KEY >/$GOOGLE_CURRENT_KEY/g" $GoogleJsonFile
+sed -i '' "s/< SECOND CLIENT ID >/$GOOGLE_SECOND_CLIENT_ID/g" $GoogleJsonFile
