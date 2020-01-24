ba792cd4c6f22f0e2f8bf5556fec7bc84b3371b6
Matt Soucoup <msoucoup@codemilltech.com>
2020-01-24 11:11:14 -0800

    app center for android

--- mod30/src/Mod30Mobile/TailwindTraders.Mobile/TailwindTraders.Mobile.Core/Helpers/AppCenterConstants.cs
+++ mod30/src/Mod30Mobile/TailwindTraders.Mobile/TailwindTraders.Mobile.Core/Helpers/AppCenterConstants.cs
@@ -6,7 +6,7 @@ namespace TailwindTraders.Mobile.Helpers
 {
     public static class AppCenterConstants
     {
-        public readonly static string AndroidAppSecret = "< ENTER YOUR APP CENTER ANDROID SECRET HERE >";
+        public readonly static string AndroidAppSecret = "d34ada1e-794f-407b-bd1d-12737ed0656c";
         public readonly static string iOSAppSecret = "< ENTER YOUR APP CENTER IOS SECRET HERE >";
     }
 }
