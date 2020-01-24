095c18b8eb30a165df6cdd0698fe4998bb717323
Matt Soucoup <msoucoup@codemilltech.com>
2020-01-23 18:34:04 -0800

    Android app center prebuild

--- mod30/src/Mod30Mobile/TailwindTraders.Mobile/TailwindTraders.Mobile.Android/appcenter-pre-build.sh
+++ mod30/src/Mod30Mobile/TailwindTraders.Mobile/TailwindTraders.Mobile.Android/appcenter-pre-build.sh
@@ -3,19 +3,29 @@
 echo "Arguments for updating:"
 echo " - iOS App Center: $IOS_APP_CENTER"
 echo " - Android App Center: $ANDROID_APP_CENTER"
-echo " - Google Project Number: $GOOGLE_PROJECT_NUMBER"
 
 # Updating ids
 
-AppCenterConstantsFile=$BUILD_REPOSITORY_LOCALPATH/src/TailwindTraders.Mobile/TailwindTraders.Mobile.Core/Helpers/AppCenterConstants.cs
-GoogleServicesFile=$BUILD_REPOSITORY_LOCALPATH/src/TailwindTraders.Mobile/TailwindTraders.Mobile.Android/google-services.json
+AppCenterConstantsFile=$BUILD_REPOSITORY_LOCALPATH/mod30/src/Mod30Mobile/TailwindTraders.Mobile/TailwindTraders.Mobile.Core/Helpers/AppCenterConstants.cs
+
+echo " - app center file: $AppCenterConstantsFile"
 
 sed -i '' "s/< ENTER YOUR APP CENTER IOS SECRET HERE >/$IOS_APP_CENTER/g" $AppCenterConstantsFile
 sed -i '' "s/< ENTER YOUR APP CENTER ANDROID SECRET HERE >/$ANDROID_APP_CENTER/g" $AppCenterConstantsFile
-sed -i '' "s/NO PROJECT NUMBER/$GOOGLE_PROJECT_NUMBER/g" $GoogleServicesFile
 
 # Print out file for reference
 cat $AppCenterConstantsFile
-cat $GoogleServicesFile
 
-echo "Updated id!"
+echo "Updated App Center Constants"
+
+GoogleJsonFile=$BUILD_REPOSITORY_LOCALPATH/mod30/src/Mod30Mobile/TailwindTraders.Mobile/TailwindTraders.Mobile.Android/google-services.json
+
+echo " - google json file: $GoogleJsonFile"
+
+sed -i '' "s/< PROJECT NUMBER >/$GOOGLE_PROJ_NUMBER/g" $GoogleJsonFile
+sed -i '' "s/< MOBILE SDK APP ID >/$GOOGLE_MOBILE_SDK_APP_ID/g" $GoogleJsonFile
+sed -i '' "s/< CLIENT ID >/$GOOGLE_CLIENT_ID/g" $GoogleJsonFile
+sed -i '' "s/< CURRENT KEY >/$GOOGLE_CURRENT_KEY/g" $GoogleJsonFile
+sed -i '' "s/< SECOND CLIENT ID >/$GOOGLE_SECOND_CLIENT_ID/g" $GoogleJsonFile
+
+cat $GoogleJsonFile
