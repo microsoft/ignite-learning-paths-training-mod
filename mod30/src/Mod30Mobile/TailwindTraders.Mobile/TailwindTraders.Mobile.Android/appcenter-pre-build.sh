4cf917b3ba6b6c1a1fce14f4530ad99037203115
Matt Soucoup <msoucoup@codemilltech.com>
2020-01-24 15:17:21 -0800

    forgot to save the changes from before

--- mod30/src/Mod30Mobile/TailwindTraders.Mobile/TailwindTraders.Mobile.Android/appcenter-pre-build.sh
+++ mod30/src/Mod30Mobile/TailwindTraders.Mobile/TailwindTraders.Mobile.Android/appcenter-pre-build.sh
@@ -23,10 +23,10 @@ GoogleJsonFile=$BUILD_REPOSITORY_LOCALPATH/mod30/src/Mod30Mobile/TailwindTraders
 echo " - google json file: $GoogleJsonFile"
 echo " - current google key: $GOOGLE_CURRENT_KEY"
 
-# sed -i '' "s/< project number >/$GOOGLE_PROJ_NUMBER/g" $GoogleJsonFile
-# sed -i '' "s/< sdk app id >/$GOOGLE_MOBILE_SDK_APP_ID/g" $GoogleJsonFile
-# sed -i '' "s/< client id >/$GOOGLE_CLIENT_ID/g" $GoogleJsonFile
+sed -i '' "s/< project number >/$GOOGLE_PROJ_NUMBER/g" $GoogleJsonFile
+sed -i '' "s/< sdk app id >/$GOOGLE_MOBILE_SDK_APP_ID/g" $GoogleJsonFile
+sed -i '' "s/< client id >/$GOOGLE_CLIENT_ID/g" $GoogleJsonFile
 sed -i '' "s/< key >/$GOOGLE_CURRENT_KEY/g" $GoogleJsonFile
-# sed -i '' "s/< client id two >/$GOOGLE_SECOND_CLIENT_ID/g" $GoogleJsonFile
+sed -i '' "s/< client id two >/$GOOGLE_SECOND_CLIENT_ID/g" $GoogleJsonFile
 
 cat $GoogleJsonFile
