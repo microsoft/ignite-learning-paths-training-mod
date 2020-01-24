abf8ea16fadd69b27d0de1823d0b31325d79342d
Matt Soucoup <msoucoup@codemilltech.com>
2020-01-24 14:59:00 -0800

    restarting the bash scripts

--- mod30/src/Mod30Mobile/TailwindTraders.Mobile/TailwindTraders.Mobile.Android/appcenter-pre-build.sh
+++ mod30/src/Mod30Mobile/TailwindTraders.Mobile/TailwindTraders.Mobile.Android/appcenter-pre-build.sh
@@ -6,26 +6,27 @@ echo " - Android App Center: $ANDROID_APP_CENTER"
 
 # Updating ids
 
-# AppCenterConstantsFile=$BUILD_REPOSITORY_LOCALPATH/mod30/src/Mod30Mobile/TailwindTraders.Mobile/TailwindTraders.Mobile.Core/Helpers/AppCenterConstants.cs
+AppCenterConstantsFile=$BUILD_REPOSITORY_LOCALPATH/mod30/src/Mod30Mobile/TailwindTraders.Mobile/TailwindTraders.Mobile.Core/Helpers/AppCenterConstants.cs
 
 # echo " - app center file: $AppCenterConstantsFile"
 
-# sed -i '' "s/< ENTER YOUR APP CENTER IOS SECRET HERE >/$IOS_APP_CENTER/g" $AppCenterConstantsFile
-# sed -i '' "s/< ENTER YOUR APP CENTER ANDROID SECRET HERE >/$ANDROID_APP_CENTER/g" $AppCenterConstantsFile
+sed -i '' "s/< ENTER YOUR APP CENTER IOS SECRET HERE >/$IOS_APP_CENTER/g" $AppCenterConstantsFile
+sed -i '' "s/< ENTER YOUR APP CENTER ANDROID SECRET HERE >/$ANDROID_APP_CENTER/g" $AppCenterConstantsFile
 
 # Print out file for reference
-# cat $AppCenterConstantsFile
+cat $AppCenterConstantsFile
 
 # echo "Updated App Center Constants"
 
-# GoogleJsonFile=$BUILD_REPOSITORY_LOCALPATH/mod30/src/Mod30Mobile/TailwindTraders.Mobile/TailwindTraders.Mobile.Android/google-services.json
+GoogleJsonFile=$BUILD_REPOSITORY_LOCALPATH/mod30/src/Mod30Mobile/TailwindTraders.Mobile/TailwindTraders.Mobile.Android/google-services.json
 
-# echo " - google json file: $GoogleJsonFile"
+echo " - google json file: $GoogleJsonFile"
+echo " - current google key: $GOOGLE_CURRENT_KEY"
 
 # sed -i '' "s/< project number >/$GOOGLE_PROJ_NUMBER/g" $GoogleJsonFile
 # sed -i '' "s/< sdk app id >/$GOOGLE_MOBILE_SDK_APP_ID/g" $GoogleJsonFile
 # sed -i '' "s/< client id >/$GOOGLE_CLIENT_ID/g" $GoogleJsonFile
-# sed -i '' "s/< key >/$GOOGLE_CURRENT_KEY/g" $GoogleJsonFile
+sed -i '' "s/< key >/$GOOGLE_CURRENT_KEY/g" $GoogleJsonFile
 # sed -i '' "s/< client id two >/$GOOGLE_SECOND_CLIENT_ID/g" $GoogleJsonFile
 
-# cat $GoogleJsonFile
+cat $GoogleJsonFile
