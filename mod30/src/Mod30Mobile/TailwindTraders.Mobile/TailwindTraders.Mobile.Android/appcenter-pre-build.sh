dbc779cc8b54eac76e2ab001642d47e0cc27baf1
Matt Soucoup <msoucoup@codemilltech.com>
2020-01-24 10:55:10 -0800

    more app center and google json

--- mod30/src/Mod30Mobile/TailwindTraders.Mobile/TailwindTraders.Mobile.Android/appcenter-pre-build.sh
+++ mod30/src/Mod30Mobile/TailwindTraders.Mobile/TailwindTraders.Mobile.Android/appcenter-pre-build.sh
@@ -18,14 +18,14 @@ cat $AppCenterConstantsFile
 
 echo "Updated App Center Constants"
 
-GoogleJsonFile=$BUILD_REPOSITORY_LOCALPATH/mod30/src/Mod30Mobile/TailwindTraders.Mobile/TailwindTraders.Mobile.Android/google-services.json
+# GoogleJsonFile=$BUILD_REPOSITORY_LOCALPATH/mod30/src/Mod30Mobile/TailwindTraders.Mobile/TailwindTraders.Mobile.Android/google-services.json
 
-echo " - google json file: $GoogleJsonFile"
+# echo " - google json file: $GoogleJsonFile"
 
-sed -i '' "s/< project number >/$GOOGLE_PROJ_NUMBER/g" $GoogleJsonFile
-sed -i '' "s/< sdk app id >/$GOOGLE_MOBILE_SDK_APP_ID/g" $GoogleJsonFile
+# sed -i '' "s/< project number >/$GOOGLE_PROJ_NUMBER/g" $GoogleJsonFile
+# sed -i '' "s/< sdk app id >/$GOOGLE_MOBILE_SDK_APP_ID/g" $GoogleJsonFile
 # sed -i '' "s/< client id >/$GOOGLE_CLIENT_ID/g" $GoogleJsonFile
-sed -i '' "s/< key >/$GOOGLE_CURRENT_KEY/g" $GoogleJsonFile
+# sed -i '' "s/< key >/$GOOGLE_CURRENT_KEY/g" $GoogleJsonFile
 # sed -i '' "s/< client id two >/$GOOGLE_SECOND_CLIENT_ID/g" $GoogleJsonFile
 
-cat $GoogleJsonFile
+# cat $GoogleJsonFile
