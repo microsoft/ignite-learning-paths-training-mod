7374e3bd20c65ce88be3a6628bde4df8500d63c0
Simon Waight <simon.waight@gmail.com>
2020-01-29 13:39:46 +1100

    Fix API URLs in appsettings.
    App Settings file in source ASP.Net Core app has overriding settings for API endpoint. This fix replaces settings values. Alternative is to delete the appsettings.json file prior / after publish and let environment variables drive experience.

--- mod10/deploy.sh
+++ mod10/deploy.sh
@@ -36,6 +36,8 @@ sleep .5
 # Publish and start application
 
 cd /tailwind/Source/Tailwind.Traders.Web
+sudo sed -i 's/http\:\/\/localhost\:5200\/v1/\/api\/v1/' appsettings.json
+sudo sed -i 's/http\:\/\/localhost\:3000/\/api/v1/' appsettings.json
 dotnet publish -c Release
 dotnet bin/Release/netcoreapp2.1/publish/Tailwind.Traders.Web.dll &
 
